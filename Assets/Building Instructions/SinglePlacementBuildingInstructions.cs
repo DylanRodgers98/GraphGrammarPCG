8bd742f202a99cafdcf4237d1a5528293589545c
Dylan Rodgers <dylanirodgers@aol.com>
2021-04-13 12:57:31 +0100

    Check for existence of relative exit points before looping through them

--- Assets/Building Instructions/SinglePlacementBuildingInstructions.cs
+++ Assets/Building Instructions/SinglePlacementBuildingInstructions.cs
@@ -6,8 +6,9 @@ public class SinglePlacementBuildingInstructions : BuildingInstructions
     public override GameObject[] Build(GameObject[] relativeSpaceObjects = null)
     {
         ValidateSpaceObjectPrefab();
-        Debug.Log($"[Single Placement Building Instructions] Space Object: {spaceObjectPrefab}");
 
+        GameObject spaceObjectPrefab = GetRandomSpaceObject();
+        
         // if no relative space objects exist, instantiate this space object at (0, 0, 0)
         // as this should be the first instantiated space object (TODO: add validation around this assumption)
         if (relativeSpaceObjects == null || relativeSpaceObjects.Length == 0)
@@ -15,37 +16,47 @@ public class SinglePlacementBuildingInstructions : BuildingInstructions
             return new[] {Instantiate(spaceObjectPrefab, Vector3.zero, Quaternion.identity)};
         }
 
-        Transform thisAttachmentPoint = GetRandomEntrancePoint(spaceObjectPrefab);
-        IList<Transform> relativeAttachmentPoints = GetExitPoints(relativeSpaceObjects);
+        Transform entrancePoint = GetRandomEntrancePoint(spaceObjectPrefab);
+        IList<Transform> relativeExitPoints = GetExitPoints(relativeSpaceObjects);
 
-        IList<Transform> otherAttachmentPoints = new List<Transform>(relativeAttachmentPoints);
-        bool isTryingNonRelativeAttachmentPoints = false;
-
-        while (otherAttachmentPoints.Count > 0)
+        IList<Transform> availableExitPoints;
+        bool isTryingNonRelativeExitPoints;
+        
+        if (relativeExitPoints.Count == 0)
         {
-            Transform otherAttachmentPoint = GetRandomAttachmentPoint(otherAttachmentPoints);
-            Quaternion spaceObjectRotation = CalculateInstantiationRotation(thisAttachmentPoint, otherAttachmentPoint);
-            GameObject instantiated = InstantiateSpaceObject(otherAttachmentPoint.position, spaceObjectRotation, thisAttachmentPoint);
+            availableExitPoints = GetNonRelativeExitPoints();
+            isTryingNonRelativeExitPoints = true;
+        }
+        else
+        {
+            availableExitPoints = new List<Transform>(relativeExitPoints);
+            isTryingNonRelativeExitPoints = false;
+        }
+
+        while (availableExitPoints.Count > 0)
+        {
+            Transform exitPoint = GetRandomAttachmentPoint(availableExitPoints);
+            Quaternion spaceObjectRotation = CalculateInstantiationRotation(entrancePoint, exitPoint);
+            GameObject instantiated = InstantiateSpaceObject(exitPoint.position, spaceObjectRotation, entrancePoint);
 
             if (instantiated != null)
             {
-                DestroyImmediate(otherAttachmentPoint.gameObject);
+                DestroyImmediate(exitPoint.gameObject);
                 return new[] {instantiated};
             }
 
-            otherAttachmentPoints.Remove(otherAttachmentPoint);
-            if (otherAttachmentPoints.Count > 0 || isTryingNonRelativeAttachmentPoints) continue;
+            availableExitPoints.Remove(exitPoint);
+            if (availableExitPoints.Count > 0 || isTryingNonRelativeExitPoints) continue;
             
-            Debug.LogWarning("Exhausted all relative attachment points. Trying non-relative attachment points.");
-            otherAttachmentPoints = GetNonRelativeAttachmentPoints(relativeAttachmentPoints);
-            isTryingNonRelativeAttachmentPoints = true;
+            availableExitPoints = GetNonRelativeExitPoints(relativeExitPoints);
+            isTryingNonRelativeExitPoints = true;
         }
 
-        throw new CannotBuildException("There are no available attachment points to attach the GameObject to, " +
+        throw new CannotBuildException("There are no available exit points to attach the GameObject to, " +
                                        $"so cannot instantiate {spaceObjectPrefab}.");
     }
 
-    private static IList<Transform> GetNonRelativeAttachmentPoints(ICollection<Transform> relativeAttachmentPoints)
+    private static IList<Transform> GetNonRelativeExitPoints(ICollection<Transform> relativeAttachmentPoints = null)
     {
         IList<Transform> nonRelativeAttachmentPoints = new List<Transform>();
         
@@ -54,7 +65,7 @@ public class SinglePlacementBuildingInstructions : BuildingInstructions
                 
         foreach (GameObject attachmentPoint in attachmentPoints)
         {
-            if (!relativeAttachmentPoints.Contains(attachmentPoint.transform))
+            if (relativeAttachmentPoints == null || !relativeAttachmentPoints.Contains(attachmentPoint.transform))
             {
                 nonRelativeAttachmentPoints.Add(attachmentPoint.transform);
             }
@@ -62,7 +73,7 @@ public class SinglePlacementBuildingInstructions : BuildingInstructions
 
         foreach (GameObject exitPoint in exitPoints)
         {
-            if (!relativeAttachmentPoints.Contains(exitPoint.transform))
+            if (relativeAttachmentPoints == null || !relativeAttachmentPoints.Contains(exitPoint.transform))
             {
                 nonRelativeAttachmentPoints.Add(exitPoint.transform);
             }
