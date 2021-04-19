ecc2ea13ac7729c148d9cdfad2bbd250c28b13d3
Dylan Rodgers <dylanirodgers@aol.com>
2021-04-19 13:16:03 +0100

    Create click-to-move functionality

---
+++ Assets/GenGra/Post-Processors/NavMeshBaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace GenGra
+{
+    public class NavMeshBaker : PostProcessor
+    {
+        public override void Process(GraphType missionGraph, IDictionary<string, GameObject[]> generatedSpace)
+        {
+            foreach (GameObject[] spaceObjectList in generatedSpace.Values)
+            {
+                foreach (GameObject spaceObject in spaceObjectList)
+                {
+                    foreach (Transform child in spaceObject.transform)
+                    {
+                        NavMeshSurface navMeshSurface = child.GetComponent<NavMeshSurface>();
+                        if (navMeshSurface != null)
+                        {
+                            navMeshSurface.BuildNavMesh();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private NavMeshAgent navMeshAgent;
     private IList<Item> inventory;
 
     public IList<Quest> Quests { get; private set; }
@@ -11,6 +14,25 @@ public class PlayerController : MonoBehaviour
     {
         inventory = new List<Item>();
         Quests = new List<Quest>();
+
+        if (navMeshAgent == null)
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) &&
+            Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100))
+        {
+            navMeshAgent.destination = hit.point;
+        }
     }
 
     public void AddItemToInventory(Item item) => inventory.Add(item);
